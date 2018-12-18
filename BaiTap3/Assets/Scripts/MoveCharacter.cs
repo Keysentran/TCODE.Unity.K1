bac759594bf77711e9aa6c8f0863ba639f96c324
Keysentran <keysentran@gamil.com>
2018-12-18 14:56:45 +0700

    Update lan 1

---
+++ BaiTap3/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+    public Transform[] target;
+    public float[] speed;
+    private float timer=0;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        target[0].Translate(Vector3.down * Time.deltaTime*speed[0]);
+        target[1].Translate(Vector3.right * Time.deltaTime * speed[1]);
+        target[2].Translate(Vector3.up * Time.deltaTime * speed[2]);
+        target[3].Translate(Vector3.left * Time.deltaTime * speed[3]);
+
+        Debug.Log(timer);
+    }
+}
