b2a84ed3ce8f778d9677bcf789f5dcabc7e98edc
Keysentran <keysentran@gamil.com>
2018-12-19 18:11:16 +0700

    Update lan 2

---
+++ BaiTap2/Assets/Scripts/ManagerObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerObject : MonoBehaviour {
+    public Transform target;
+    public Vector3 translation;
+    public Vector3 rotation;
+    public Vector3 scale;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        target.position = translation;
+        target.eulerAngles = rotation;
+        target.localScale = scale;
+    }
+}
--- BaiTap3/Assets/Scripts/MoveCharacter.cs
+++ BaiTap3/Assets/Scripts/MoveCharacter.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour {
-    public Transform[] target;
+    public Transform[] positions;
+    public GameObject[] objects;
     public float[] speed;
     private float timer=0;
+    private void Start()
+    {
+        for (int i = 0; i < positions.Length; i++)
+        {
+            Instantiate(objects[i], positions[i]);
+        }
+    }
     private void Update()
     {
-        timer += Time.deltaTime;
-        target[0].Translate(Vector3.down * Time.deltaTime*speed[0]);
-        target[1].Translate(Vector3.right * Time.deltaTime * speed[1]);
-        target[2].Translate(Vector3.up * Time.deltaTime * speed[2]);
-        target[3].Translate(Vector3.left * Time.deltaTime * speed[3]);
 
-        Debug.Log(timer);
+        positions[0].Translate(Vector3.down * Time.deltaTime*speed[0]);
+        positions[1].Translate(Vector3.right * Time.deltaTime * speed[1]);
+        positions[2].Translate(Vector3.up * Time.deltaTime * speed[2]);
+        positions[3].Translate(Vector3.left * Time.deltaTime * speed[3]);
+
     }
 }
